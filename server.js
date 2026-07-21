c15a902e82d9acf33184233e069561fd9f993d57
Manthan Rupapara <manthanrupapara99@gmail.com>
2026-07-21 15:33:37 +0530

    Whatsapp Web Shared
    Whatsapp Web Shared

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const customerOrderRoutes = require("./routes/customerOrderRoutes");
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Server running...");
